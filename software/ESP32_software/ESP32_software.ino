ed4efe6ba0ce37be80629c5d2c8283ab2e601e09
Ercik H L Murrugarra <leonmurrugarra29@gmail.com>
2025-01-29 10:40:35 -0500

    comunicacion con firebase

--- software/ESP32_software/ESP32_software.ino
+++ software/ESP32_software/ESP32_software.ino
@@ -2,7 +2,6 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <FirebaseESP32.h>
-//#include <Firebase_ESP_Client.h>  // Nueva biblioteca
 #include <addons/TokenHelper.h> // Provide the token generation process info.
 #include <addons/RTDBHelper.h> // Provide the RTDB payload printing info and other helper functions.
 #include <time.h>
@@ -18,6 +17,28 @@ const char* password = "Murrugarra.29";
 IPAddress local_IP(192, 168, 0, 232);       // IP fija del ESP32
 IPAddress gateway(192, 168, 0, 1);         // Puerta de enlace (IP del router)
 IPAddress subnet(255, 255, 255, 0);        // Máscara de subred
+IPAddress primaryDNS(8, 8, 8, 8);       // DNS de Google
+IPAddress secondaryDNS(8, 8, 4, 4);     // DNS de Google
+
+// Configurar el servidor NTP
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = -18000;  // Ajusta según tu zona horaria (ejemplo: -5 horas)
+const int   daylightOffset_sec = 3600;
+
+// Firebase credenciales
+/*
+#define FIREBASE_AUTH "Zih9fwFyk0UZ1t6oJBlpajSVn9EaZXBLHQ4ArlfX"
+#define FIREBASE_PROJECT_ID "sistema-de-riego-iot-76aa6"
+ */
+#define API_KEY "AIzaSyBUOzqsE50IHq4HRxlkcT1GZp0lmlmS4mM"
+#define DATABASE_URL "https://sistema-de-riego-iot-76aa6-default-rtdb.firebaseio.com" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+#define USER_EMAIL "bacasiones20@gmail.com"
+#define USER_PASSWORD "Murrugarra.29"
+
+// Define Firebase Data object
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
 
 //seccion de webserver
 WebServer server(80);
@@ -35,23 +56,9 @@ void handleData() {
 
 }
 
-// Firebase credenciales
-#define FIREBASE_HOST "https://sistema-de-riego-iot-76aa6-default-rtdb.firebaseio.com"
-#define FIREBASE_AUTH "Zih9fwFyk0UZ1t6oJBlpajSVn9EaZXBLHQ4ArlfX"
-#define FIREBASE_PROJECT_ID "sistema-de-riego-iot-76aa6"
-#define FIREBASE_API_KEY "AIzaSyBUOzqsE50IHq4HRxlkcT1GZp0lmlmS4mM"
-#define FIREBASE_USER_EMAIL "bacasiones20@gmail.com"
-#define FIREBASE_USER_PASSWORD "Murrugarra.29"
-
-// Define Firebase Data object
-FirebaseData fbdo;
-FirebaseAuth auth;
-FirebaseConfig config;
-
 // Variable para controlar el tiempo de actualización
-unsigned long lastUpdateTime = 0;
-const unsigned long UPDATE_INTERVAL = 5000; // 5 segundos
-
+unsigned long sendDataPrevMillis = 0;
+unsigned long count = 0;
 // Función para obtener timestamp
 String getTimestamp() {
   struct tm timeinfo;
@@ -76,8 +83,11 @@ void setup() {
   pinMode(lluviaSensorPin, INPUT);
   pinMode(waterLevelSensorPin, INPUT);
 
+  // Configurar la hora desde el servidor NTP
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
   // Configurar IP fija antes de conectar a Wi-Fi
-  if (!WiFi.config(local_IP, gateway, subnet)) {
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
     Serial.println("Error al configurar IP fija");
   }
   // Connect to Wi-Fi
@@ -89,25 +99,34 @@ void setup() {
   }
   Serial.println();
   Serial.println("Connectado al Wi-Fi");
+  // Esperar hasta obtener la hora
+    struct tm timeinfo;
+    while (!getLocalTime(&timeinfo)) {
+        Serial.println("⚠️ Esperando sincronización con NTP...");
+        delay(1000);
+    }
+    Serial.println("✅ Hora sincronizada correctamente.");
 
 //inicializando servidor
   server.on("/sendData", HTTP_POST, handleData);  // Ruta para recibir datos
   server.begin();
   Serial.println("Servidor HTTP iniciado");
 
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
    // Configuración Firebase
-  config.api_key = FIREBASE_API_KEY;
-  config.database_url = FIREBASE_HOST;
-  config.signer.tokens.legacy_token = FIREBASE_AUTH;
-  auth.user.email = FIREBASE_USER_EMAIL;
-  auth.user.password = FIREBASE_USER_PASSWORD;
+  config.api_key = API_KEY;
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+  config.database_url = DATABASE_URL;
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
 
+  Firebase.reconnectNetwork(true);
   fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
-
-  Firebase.reconnectWiFi(true);
-  //fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
-  config.timeout.socketConnection = 10 * 1000;
+  //config.timeout.socketConnection = 10 * 1000;
   Firebase.begin(&config, &auth);
+    Firebase.setDoubleDigits(5);
 
  
 }
@@ -126,8 +145,6 @@ void loop() {
     int waterLevelSensorValue = analogRead(waterLevelSensorPin);
     int humedad = String(data).toInt();
     //resibe datos de clientes
-    server.handleClient();
-    //Serial.println(WiFi.localIP());
     // imprime los datos en el serial
     Serial.print("Sensor de llubia: ");
     Serial.println(lluviaSensorValue);
@@ -148,9 +165,18 @@ void loop() {
     sensorData.set("rain/history/" + timestamp + "/timestamp", timestamp);
     sensorData.set("waterLevel/history/" + timestamp + "/value", waterLevelSensorValue);
     sensorData.set("waterLevel/history/" + timestamp + "/timestamp", timestamp);
-    sensorData.set("/humidity/history/" + timestamp + "value", humedad);
+    sensorData.set("/humidity/history/" + timestamp + "/value", humedad);
     sensorData.set("/humidity/history/" + timestamp +"/timestamp", timestamp);
-    
+
+   /*  
+    if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
+      {
+        sendDataPrevMillis = millis(); 
+
+        Serial.println("firebase conectado");
+            count++;
+      }
+     */
     // Guardar datos en Firebase
        // Actualizar Firebase
     if (Firebase.updateNode(fbdo,F("/sensors"), sensorData)) {
@@ -180,5 +206,5 @@ void loop() {
    
   
   // Delay before next loop iteration
-  delay(200);
+  //delay(200);
 }
